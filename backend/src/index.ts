b0a643a84ca61e4f0a846b0c206127ab505d1813
peu77 <emilebert@icloud.com>
2022-07-24 15:07:53 +0200

    remove remix and use nextjs

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,10 +2,12 @@ import {AppDataSource} from "./data-source"
 import {initControllers} from "@peu77/expresswrapper";
 import express from "express"
 import {LinkController} from "./controller/user/LinkController";
+import cors from "cors";
 
 AppDataSource.initialize().then(async () => {
     console.log("Database initialized")
     const app = express()
+    app.use(cors())
 
     app.listen(3000, () => {
         initControllers(app, [
